3653f643bf8dac2800b3f7b092756950e9ecbc5a
remzelk <aliciamaye.mondero@letran.edu.ph>
2022-02-10 01:40:27 +0800

    Initial commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Profile</title>
+<link rel="stylesheet" type="text/css" href="/css/StyleSheet.css" >
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+<!------ Include the above in your HEAD tag ---------->
+<body>
+<div class="container">
+<div class="row">
+      <div class="col-xs-12 col-sm-9">
+        
+        <!-- User profile -->
+        <div class="panel panel-default">
+          <div class="panel-heading">
+          <h4 class="panel-title">User Profile</h4>
+          </div>
+          <div class="panel-body">
+            <div class="profile__avatar">
+              <img src="https://scontent.fmnl30-1.fna.fbcdn.net/v/t39.30808-6/233796593_1983105245200409_8071716225380599380_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=XEJAOO-jssAAX9zaCUN&_nc_ht=scontent.fmnl30-1.fna&oh=00_AT9D1V7vH1Sk-3ITXx2UUkEKx_T5vWGZwabDkCiMcIirzQ&oe=62084A6D" alt="...">
+            </div>
+            <div class="profile__header">
+              <h4>Alicia Maye A. Mondero <small>Information Technology Student</small></h4>
+              <p class="text-muted">
+                I like playing video games, listening to music, drawing, memes, watching anime and cartoons, animals, my friends, and Sean Robert McGaffey!
+              </p>
+              <p>
+                <b>Social links: </b>&ensp;
+                <a href="https://www.facebook.com/EveryRegionPokemonMaster/">Facebook</a> &ensp;   
+                <a href="https://www.twitter.com/kurezuma/">Twitter</a> &ensp;    
+                <a href="https://steamcommunity.com/id/remzelk/">Steam</a> &ensp;
+                <a href="https://open.spotify.com/user/22vbui3isk3uobesaqujgqcmy">Spotify</a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <!-- User info -->
+        <div class="panel panel-default">
+          <div class="panel-heading">
+          <h4 class="panel-title">User Information</h4>
+          </div>
+          <div class="panel-body">
+            <table class="table profile__table">
+              <tbody>
+                <tr>
+                  <th><strong>Location</strong></th>
+                  <td>Makati, Philippines</td>
+                </tr>
+                <tr>
+                  <th><strong>Birthday</strong></th>
+                  <td>November 2, 1999</td>
+                </tr>
+                <tr>
+                  <th><strong>Age</strong></th>
+                  <td>22 years old</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <!-- Education -->
+        <div class="panel panel-default">
+          <div class="panel-heading">
+          <h4 class="panel-title">Education</h4>
+          </div>
+          <div class="panel-body">
+            <table class="table profile__table">
+              <tbody>
+                <tr>
+                  <th><strong>Studies at</strong></th>
+                  <td>Colegio de San Juan de Letran</td>
+                </tr>
+                <tr>
+                  <th><strong>Studies</strong></th>
+                  <td>Bachelor of Science in Information Technology</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <!-- Relationships -->
+        <div class="panel panel-default">
+          <div class="panel-heading">
+          <h4 class="panel-title">Relatives</h4>
+          </div>
+          <div class="panel-body">
+            <table class="table profile__table">
+              <tbody>
+                <tr>
+                  <th><strong>Mother</strong></th>
+                  <td>Vivian A. Mondero</td>
+                </tr>
+                <tr>
+                  <th><strong>Father</strong></th>
+                  <td>Rodolfo E. Mondero Jr.</td>
+                </tr>
+                <tr>
+                  <th><strong>Sibling</strong></th>
+                  <td>Andrea Monique A. Mondero</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+</body>
+</html>        
